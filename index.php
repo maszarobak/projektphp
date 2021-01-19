fe45a336ce5589d313f8d0c4d49fb6e4ee1aba54
maszarobak <mariia.robaczewska@gmail.com>
2021-01-19 17:12:51 +0100

    baza danych

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php include "database.php"; ?>
+<?php
+    //wyciagam wszystkie wpisy
+    $query ="SELECT * FROM  opis";
+    $opis = mysqli_query($con, $query);
+
+?>
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -10,23 +17,28 @@
 <body>
 <?php require "blocks/header.php" ?>
 <div class="container mt-5">
-    <h3 class="mb-5">Ciekawe strony</h3>
+    <h3 class="mb-5">Najlepsze paletki</h3>
     <div class="d-flex flex-wrap">
    <?php
    for($i = 0; $i<5; $i++):
     ?>
-    <div class="card mb-4 shadow-sm">
+    <div id=opis class="card mb-4 shadow-sm">
         <div class="card-header">
             <h4 class="my-0 font-weight-normal">Tekst</h4>
         </div>
-        <div class="card-body">
+        <div  class="card-body">
             <img src="img/<?php echo ($i+1) ?>.jpg" class="img-thumbnail">
             <ul class="list-unstyled mt-3 mb-4">
-          
-                <li>dsvsdv</li>
-                <li>2dsv</li>
-                <li>Emdsvpport</li>
-                <li>Helpdvaccess</li>
+           
+            <?php while($wiersz = mysqli_fetch_assoc($opis)) : ?>
+                    <li class="opisy"><span class="nazwa">
+                    <?php echo $wiersz["nazwa"] ?> </span>
+                    <?php echo $wiersz["firma"] ?>
+                     <?php echo $wiersz["kraj"] ?>
+                     <?php echo $wiersz["cena"] ?></li>
+                
+                
+                <?php endwhile; ?>
                 
             </ul>
             <button type="button" class="w-100 btn btn-lg btn-outline-primary">Więcej</button>
